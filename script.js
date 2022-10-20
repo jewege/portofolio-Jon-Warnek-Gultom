ea8f1af5ee647a8aded5bb4691205e53783924a7
jonw93 <jonwarnekgultom@gmail.com>
2022-10-20 12:55:51 +0700

    commit pertama portofolio

---
+++ script.js
@@ -0,0 +1,60 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+function reloadpage() {
+  location.reload();
+}
+
+function redirIg() {
+  window.location.href = "https://instagram.com/uuarneck_gltm";
+}
+
+function redirWa() {
+  window.location.href = "https://wa.me/082366935302";
+}
+
+function redirFb() {
+  window.location.href = "https://www.facebook.com/uuarnek.gltm/";
+}
+function redirGH() {
+  window.location.href = "https://github.com/jewege";
+}
+
+function contact() {
+  window.location.href = "mailto:jonwarnekgultom@gmail.com";
+}
+
+function project1() {
+  window.location.href = "https://youtu.be/m1EuO-vsivE";
+}
+
+function project2() {
+  window.location.href = "https://youtu.be/kK6J3loAGYs";
+}
+
+function project3() {
+  window.location.href = "https://github.com/jewege/Jam-Digital";
+}
+
+function project4() {
+  window.location.href =
+    "https://github.com/jewege/Membuat-Navbar-responsive-dengan-menggunakan-HTML-CSS-JavaScript";
+}
+
+function project5() {
+  window.location.href = "https://github.com/jewege/Contoh-Company-Profile";
+}
+
+function project6() {
+  window.location.href =
+    "https://github.com/jewege/Calculator-dengan-HTML-CSS-JavaScript";
+}
+function project7() {
+  window.location.href =
+    "https://github.com/jewege/Todolist-Yang-sangat-sederhana-dengan-Javascript";
+}
