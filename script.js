22b1f0d2bea34ae1e2209f1c28b1b8831e3f940b
jonw93 <jonwarnekgultom@gmail.com>
2022-11-28 13:51:32 +0700

    New Background

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
